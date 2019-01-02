906924fe60f6499d80687622f8558be57d78f946
Paul Blischak <paul.blischak@gmail.com>
2019-01-02 15:02:13 -0700

    testing reading dht and data pin

--- configuration.py
+++ configuration.py
@@ -5,14 +5,14 @@ from sys import exit, argv
 from GMPi_Pack import BuildConfig
 
 print("Making configuration file: config.txt")
-
+print(len(argv))
 try:
 	if len(argv) == 3:
 		dht      = argv[1]
 		data_pin = argv[2]
 	elif len(argv) == 4:
-		dht      = argv[1]
-		data_pin = argv[2]
+		dht      = argv[2]
+		data_pin = argv[3]
 	else:
 		print("ERROR: Unable to parse DHT and Data Pin (should be integers)...\n")
 		print("Usage:")
